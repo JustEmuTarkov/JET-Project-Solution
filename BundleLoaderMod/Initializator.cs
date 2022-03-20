61b12d72427e8ba7bf796b62e1ec1013504ebe61
TheMaoci <themaoci@gmail.com>
2022-03-20 20:11:46 +0100

    bundle loader start initialization (remove commented inits)

--- BundleLoaderMod/Initializator.cs
+++ BundleLoaderMod/Initializator.cs
@@ -14,7 +14,9 @@ namespace BundleLoaderMod
         protected override void Initialize(IReadOnlyDictionary<Type, JetMod> dependencies, string gameVersion)
         {
             // this mod is still in progress and is not finished !!!!
-
+            JET.Utility.Patching.HarmonyPatch.Patch<Patches.BundleLoad>();
+            JET.Utility.Patching.HarmonyPatch.Patch<Patches.EasyAssets>();
+            JET.Utility.Patching.HarmonyPatch.Patch<Patches.EasyBundle>();
             //JET.Utility.Patching.HarmonyPatch.Patch<Patches.DisplayMoneyPanel>();
         }
         public static string OriginalCreator = "AppeazeTheCheese";
